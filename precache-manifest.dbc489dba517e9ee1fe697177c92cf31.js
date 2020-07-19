3d6472d02f690e3d4b7e4373414c7de9e26668d9
kimchankwon <kimchankwon@gmail.com>
2020-07-20 01:19:51 +1000

    Updates

---
+++ precache-manifest.dbc489dba517e9ee1fe697177c92cf31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9330456fb1b2815f52ed704fc1933060",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13ed74ffce48588bd179",
+    "url": "/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "2f246c9d73a92cf038f2",
+    "url": "/static/css/main.012a19cf.chunk.css"
+  },
+  {
+    "revision": "13ed74ffce48588bd179",
+    "url": "/static/js/2.e8ac3b92.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e8ac3b92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f246c9d73a92cf038f2",
+    "url": "/static/js/main.cfce7c0f.chunk.js"
+  },
+  {
+    "revision": "7e8c3573683f4f7304a2",
+    "url": "/static/js/runtime-main.404d0bfe.js"
+  }
+]);
