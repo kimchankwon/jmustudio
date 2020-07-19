a83872d2a018d1633900fa1b5aec12647d7acdd8
kimchankwon <kimchankwon@gmail.com>
2020-07-20 01:07:27 +1000

    Updates

--- precache-manifest.fef5b78f39db5fec83c7369fa93023ba.js
+++ precache-manifest.fef5b78f39db5fec83c7369fa93023ba.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "735266f1e67ae872a4840ede4d523377",
+    "revision": "9d0c7aa89695c9173fa5b5515b7c9221",
     "url": "/jmustudio/index.html"
   },
   {
-    "revision": "a7f452875f46c6bf0244",
+    "revision": "087058cfad7c18bd3fc7",
     "url": "/jmustudio/static/css/2.4c4477be.chunk.css"
   },
   {
-    "revision": "32ff11e4329464c7a8b8",
+    "revision": "c804d6c1ecad5b2675e3",
     "url": "/jmustudio/static/css/main.012a19cf.chunk.css"
   },
   {
-    "revision": "a7f452875f46c6bf0244",
-    "url": "/jmustudio/static/js/2.64492628.chunk.js"
+    "revision": "087058cfad7c18bd3fc7",
+    "url": "/jmustudio/static/js/2.5df9af7f.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/jmustudio/static/js/2.64492628.chunk.js.LICENSE.txt"
+    "url": "/jmustudio/static/js/2.5df9af7f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "32ff11e4329464c7a8b8",
-    "url": "/jmustudio/static/js/main.4e49dfaa.chunk.js"
+    "revision": "c804d6c1ecad5b2675e3",
+    "url": "/jmustudio/static/js/main.1638c052.chunk.js"
   },
   {
     "revision": "06c337c21b5cae6c7bb5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jmustudio/precache-manifest.86e2a7a4bebbf6a9dbac145158fdbbcb.js"
+  "/jmustudio/precache-manifest.fef5b78f39db5fec83c7369fa93023ba.js"
 );
 
 self.addEventListener('message', (event) => {
